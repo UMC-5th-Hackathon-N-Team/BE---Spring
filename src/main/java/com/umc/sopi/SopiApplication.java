e8f9e2bd7c2d8e481af54f2a72753d281f7760da
jinuk <cimaron713@gmail.com>
2024-01-04 15:05:19 +0900

    initial

---
+++ src/main/java/com/umc/sopi/SopiApplication.java
@@ -0,0 +1,13 @@
+package com.umc.sopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopiApplication.class, args);
+    }
+
+}
