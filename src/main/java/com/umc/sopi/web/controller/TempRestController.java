8428bc895aecbcade156b9d5b76c35215932b008
jinuk <cimaron713@gmail.com>
2024-01-04 15:50:44 +0900

    feat: API 응답 통일 & 에러 핸들러

---
+++ src/main/java/com/umc/sopi/web/controller/TempRestController.java
@@ -0,0 +1,21 @@
+package com.umc.sopi.web.controller;
+
+import com.umc.sopi.apiPayload.ApiResponse;
+import com.umc.sopi.web.converter.TempConverter;
+import com.umc.sopi.web.dto.TempResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/temp")
+@RequiredArgsConstructor
+public class TempRestController {
+
+    @GetMapping("/test")
+    public ApiResponse<TempResponse.TempTestDTO> testAPI(){
+
+        return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+}
