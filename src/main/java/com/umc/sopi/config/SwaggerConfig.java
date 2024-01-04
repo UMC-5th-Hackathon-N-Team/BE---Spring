5c4775f6a144765e5e97b764ecb4d459605c1f6e
jinuk <cimaron713@gmail.com>
2024-01-04 16:27:57 +0900

    feat: swagger config 추가

---
+++ src/main/java/com/umc/sopi/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.umc.sopi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "SOPI API 명세서",
+                description = "소중한 피!",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("SOPI API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
