57299717c6ea9b78f00611fa375ebd43b6285951
sh <dldh0917@naver.com>
2024-01-04 20:10:43 +0900

    entity 설계

---
+++ src/main/java/com/umc/sopi/apiPayload/domain/User.java
@@ -0,0 +1,30 @@
+package com.umc.sopi.apiPayload.domain;
+
+import com.umc.sopi.apiPayload.domain.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime birth;
+
+    private String blood;
+
+    private Long draw;
+}
---
+++ src/main/java/com/umc/sopi/apiPayload/domain/UserBlood.java
@@ -0,0 +1,31 @@
+package com.umc.sopi.apiPayload.domain;
+
+import com.umc.sopi.apiPayload.domain.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserBlood extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+
+    private Long number;
+
+    private LocalDateTime date;
+
+    private String location;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
